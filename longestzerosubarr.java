8183b7f1d62723e6d8c989a2e1cc9527210db5c0
iamjha007 <iamjha007@gmail.com>
2022-05-01 22:28:28 +0530

    day4 Q-4

---
+++ longestzerosubarr.java
@@ -0,0 +1,19 @@
+int maxLen(int arr[], int n)
+    {
+        HashMap<Integer,Integer> map=new HashMap<>();
+        int sum=0;
+        int maxlen=0;
+        for(int i=0;i<n;i++){
+           sum=sum+arr[i];
+           if(sum==0)
+             maxlen=i+1;
+            else
+           if(map.containsKey(sum))
+             {
+              maxlen=Math.max(maxlen,i-map.get(sum));  
+             }
+             else
+             map.put(sum,i);
+        }
+        return maxlen;
+    }
